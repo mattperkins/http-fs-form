9f90e9ddd0d49d1fe961f1611e763c483e3c8030
mattperkins <github@mattperkins.com>
2018-11-28 22:50:21 +0000

    add test html to response

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const http = require('http')
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers)
   // process.exit()
-  return res.end()
+  res.setHeader('Content-type', 'text/html')
+  res.write('<html>')
+  res.write('<head><title>App</title></head>')
+  res.write('<body><h1>App</h1></body>')
+  res.write('</html>')
+  res.end()
 })
 
 server.listen(3004, () => {
