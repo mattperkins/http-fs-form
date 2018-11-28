b5d8214239879d06c02332a7e7d57550cbeaed59
mattperkins <github@mattperkins.com>
2018-11-28 22:59:44 +0000

    add test form html

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers)
+  // console.log(req.url, req.method, req.headers)
   // process.exit()
+  const url = req.url
+  if (url === '/') {
+    res.write('<html>')
+    res.write('<head><title>Enter Message</title></head>')
+    res.write('<body><form action="/message" method="POST">')
+    res.write('<input type="text" name="message">')
+    res.write('<button type="submit">Send</button>')
+    res.write('</form></body>')
+    res.write('</html>')
+    res.end()
+  }
   res.setHeader('Content-type', 'text/html')
   res.write('<html>')
   res.write('<head><title>App</title></head>')
