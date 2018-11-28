f33e85d36f44f1027feaf6a5c9ec3c9dd0a68557
mattperkins <github@mattperkins.com>
2018-11-28 21:32:16 +0000

    scaffold http server

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const http = require('http')
 
-http.createServer((req,res) => {
+const server = http.createServer((req, res) => {
   console.log(req)
+  return res.end()
+})
+
+server.listen(3003, () => {
+  console.log('listening...')
 })
