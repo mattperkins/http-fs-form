3dc9d38bd4b1e7be7f6d2813e49084bae39a107a
mattperkins <github@mattperkins.com>
2018-11-28 23:16:02 +0000

    conditional logic for form submit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
   // console.log(req.url, req.method, req.headers)
   // process.exit()
   const url = req.url
+  const method = req.method
   if (url === '/') {
     res.write('<html>')
     res.write('<head><title>Enter Message</title></head>')
@@ -14,10 +16,15 @@ const server = http.createServer((req, res) => {
     res.write('</html>')
     return res.end()
   }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', '...')
+    res.statusCode = 302
+    res.setHeader('Location', '/')
+  }
   res.setHeader('Content-type', 'text/html')
   res.write('<html>')
-  res.write('<head><title>App</title></head>')
-  res.write('<body><h1>App</h1></body>')
+  res.write('<head><title>Message Sent</title></head>')
+  res.write('<body><h1>Message Sent</h1></body>')
   res.write('</html>')
   res.end()
 })
